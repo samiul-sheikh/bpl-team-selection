ee3d29f3040d5e2f569127d3fadf4151a7aa2670
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-01 20:01:17 +0600

    display more information in Player Component

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import './Player.css';
 
 const Player = (props) => {
-    console.log(props.player);
+    // console.log(props.player);
+    const { image, name, role, salary, id } = props.player;
+
     return (
-        <div>
-            {/* <h3>This is player</h3> */}
-            <h4>{props.player.name}</h4>
-            <p>{props.player.role}</p>
+        <div className="player">
+            <div>
+                <img src={image} alt="" />
+            </div>
+            <div>
+                <h2>{name}</h2>
+                <h4>Role: {role}</h4>
+                <h3>Salary: {salary}</h3>
+                <h5>Jersey No: {id}</h5>
+            </div>
         </div>
     );
 };
