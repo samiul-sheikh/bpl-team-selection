d9d8d895cd1eff7480ffd161ac5d9af4645aa08b
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-01 20:34:59 +0600

    adding third party library, font awesome, react-fontawesome

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Player.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Player = (props) => {
     // console.log(props.player);
-    const { image, name, role, salary, id } = props.player;
+    const { image, name, role, salary } = props.player;
 
     return (
         <div className="player">
@@ -13,8 +15,8 @@ const Player = (props) => {
             <div>
                 <h2>{name}</h2>
                 <h4>Role: {role}</h4>
-                <h3>Salary: {salary}</h3>
-                <h5>Jersey No: {id}</h5>
+                <h4>Salary: {salary}</h4>
+                <button className="hire-button"><FontAwesomeIcon icon={faUserPlus} />Hire Player</button>
             </div>
         </div>
     );
